41176c9b0f28e8e0c8b22302fb85b85969b7d4c0
muhamadarsaludin <muhamadarsaludin71@gmail.com>
2021-02-16 14:35:08 +0700

    add style

--- app/Views/admin/dashboard.php
+++ app/Views/admin/dashboard.php
@@ -2,28 +2,28 @@
 
 <?= $this->section('content'); ?>
 
-<img src="<?= base_url('img/jumbotron-admin.jpg'); ?>" class="img-fluid mb-4" alt="Jumbotron image">
-
 <!-- Content Row -->
 <div class="row">
 
+<img src="<?= base_url('img/jumbotron-admin.jpg'); ?>" class="img-fluid mb-4" alt="Jumbotron image">
     <!-- Earnings (Monthly) Card Example -->
     <div class="col-xl-3 col-md-6 mb-4">
-        <div class="card bg-gradient-main shadow-wild-watermelon border-0 h-100 py-2">
-            <div class="card-body text-white">
+        <div class="card shadow h-100 border-0 py-2">
+            <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col-auto mr-3">
-                        <i class="fas fa-calendar fa-2x "></i>
+                        <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
                     </div>
                     <div class="col">
-                        <div class="text-xs font-weight-bold text-uppercase mb-1">
-                            Earnings (Monthly)</div>
-                        <div class="h5 mb-0 font-weight-bold">Rp. 21.000.000</div>
+                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                            Earnings (Annual)</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. 103.000.000</div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 
     <!-- Earnings (Monthly) Card Example -->
     <div class="col-xl-3 col-md-6 mb-4">
@@ -82,13 +82,6 @@
                 </div>
             </div>
         </div>
-    </div>
-</div>
-
-<div class="container-fluid content-frame shadow">
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="content-heading mb-0 text-gray-800">Transaction</h1>
     </div>
 </div>
 <?= $this->endSection(); ?>
